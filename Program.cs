9875e8983190866c64458cc28a62a19c1c103e8a
Shivam Sharma <shivam.piyush99@gmail.com>
2022-02-17 07:53:01 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Greet()
+        {
+            Console.WriteLine(" Welcome bro!");
+        }
+        public void name()
+        { 
+            Console.WriteLine(" Rahul !"); 
+        }
+        public static void msg()
+        {
+            Console.WriteLine(" This is last method !");
+        }
+        static void Main(string[] args)
+        {
+            Greet();
+
+            Program obj1 = new Program();
+            obj1.name();
+
+            Program.msg();
+
+            //int[,] arr = new int[,] { { 12,45,78},{ 12,45,78},{ 12,45,78}, };
+            //Console.WriteLine("Printing Matrix: ");
+            //int m = 2;
+            //int n = 2;
+            //for (int i = 0; i <= m; i++)
+            //{
+            //    for (int j = 0; j <= n; j++)
+            //    {
+            //        Console.Write(arr[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+            Console.ReadLine();
+        }
+    }
+}
